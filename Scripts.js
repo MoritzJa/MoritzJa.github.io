a69383732141237292d818db58fbbdc38ae92fa7
Moritz <moritz.jarosch@gmail.com>
2021-10-11 23:42:35 +0200

    Complete redesign of the site

---
+++ Scripts.js
@@ -0,0 +1,35 @@
+function flipME(event){
+	var element = event.currentTarget;
+	if (element.className === "ME-inner") {
+		if(element.style.transform == "rotateY(180deg)") {
+		element.style.transform = "rotateY(0deg)";
+    }
+    else {
+      element.style.transform = "rotateY(180deg)";
+    }
+  }
+};
+
+function flipT(event){
+	var element = event.currentTarget;
+	if (element.className === "T-inner") {
+		if(element.style["-webkit-transform"] == "rotate3d(1, -1, 0, 180deg)") {
+		element.style["-webkit-transform"] = "rotate3d(1,-1, 0,0deg)";
+    }
+    else {
+      element.style["-webkit-transform"] = "rotate3d(1,-1, 0,180deg)";
+    }
+  }
+};
+
+function flipC(event){
+	var element = event.currentTarget;
+	if (element.className === "C-inner") {
+		if(element.style["-webkit-transform"] == "rotate3d(1, 1, 0, 180deg)") {
+		element.style["-webkit-transform"] = "rotate3d(1,1, 0,0deg)";
+    }
+    else {
+      element.style["-webkit-transform"] = "rotate3d(1,1, 0,180deg)";
+    }
+  }
+};
